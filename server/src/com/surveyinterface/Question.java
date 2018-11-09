fa9995bf983456ef53df1171b454374e46950d40
chamodya <chamodyawimansha@gmail.com>
2018-11-09 20:18:01 +0000

    some interfaces

---
+++ server/src/com/surveyinterface/Question.java
@@ -0,0 +1,17 @@
+package com.surveyinterface;
+/**
+ * Questions and their answers
+ * @author chamo
+ *
+ */
+public class Question {
+	
+	private String question;
+	private String[] answers;
+
+	public Question(String question, String[] answers) {
+		this.question = question;
+		this.answers = answers;
+	}
+
+}
