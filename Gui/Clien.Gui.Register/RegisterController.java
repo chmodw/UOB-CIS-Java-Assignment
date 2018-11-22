f234c7cc33b835e68cdc1df6bac622f3c9e67077
Rashinda Sandaru <rashindasandaru.hal@gmail.com>
2018-11-22 07:25:32 +0000

    Replace RegisterController.java

--- Gui/Clien.Gui.Register/RegisterController.java
+++ Gui/Clien.Gui.Register/RegisterController.java
@@ -1,5 +1,24 @@
 package application.register;
 
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
+
 public class RegisterController {
 
+	@FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXTextField password;
+
+    @FXML
+    private JFXTextField passwordcheck;
+
+    @FXML
+    private JFXRadioButton bTester;
+
+    @FXML
+    private JFXRadioButton developer;
 }
