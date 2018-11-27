3c906c0483b31d7c0afdb44a385dbf8bbdd47a49
Rashinda Sandaru <rashindasandaru.hal@gmail.com>
2018-11-27 16:25:01 +0000

    Replace RegisterController.java // updated gui

--- Gui/Clien.Gui.Register/RegisterController.java
+++ Gui/Clien.Gui.Register/RegisterController.java
@@ -16,9 +16,4 @@ public class RegisterController {
     @FXML
     private JFXTextField passwordcheck;
 
-    @FXML
-    private JFXRadioButton bTester;
-
-    @FXML
-    private JFXRadioButton developer;
 }
