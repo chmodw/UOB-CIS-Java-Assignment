307d4b3952d6aac93c1745084dbdafda56232683
chamodya wimansha <chamodyawimansha@gmail.com>
2018-12-08 18:19:10 +0530

    SAR

---
+++ Application_2.1/Server_2.1/src/application/Question.java
@@ -0,0 +1,116 @@
+package application;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import utils.SentimentAnalysis;
+
+/**
+ * Question Class
+ * this class will create new question objects and answer object
+ * @author chamo
+ *
+ */
+
+public class Question {
+	
+	private String id;
+	private String question;
+	private String is_active;
+	private String created_on;
+	
+	private String answer;
+	private String user_email;
+	private String answerd_on;
+	
+	// sentimental analysis results
+	private String SAR;
+	
+	/**
+	 * New question
+	 * 
+	 * @param question
+	 */
+	public Question(String question, boolean active) {
+		this.id = qid(question);
+		this.question = question;
+		this.is_active = String.valueOf(active); //convert boolean to string
+		this.created_on = now();
+	}
+	
+	/**
+	 * Answered question
+	 * 
+	 * @param questionID
+	 * @param answer
+	 * @param userEmail
+	 */
+	public Question(String questionID, String answer, String userEmail) {
+		this.id = questionID;
+		this.answer = answer;
+		this.user_email = userEmail;
+		this.answerd_on = now();
+		
+		if(questionID.equals("usercomment")) {
+			try {
+				this.SAR = generateSAR(URLEncoder.encode(answer, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				// e.printStackTrace();
+				System.out.println("Error! Question URL encoding");
+			}
+		}
+		
+	}
+	
+	private String qid(String question) {
+		// generate question id by removing all characters other than letters
+		return question.replaceAll("[^A-Za-z]+", "");
+	}
+	
+	private String now() {
+		//get the current data and time
+		return DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
+	}
+	
+	private String generateSAR(String answer) {		
+		//get the sentimental result from the user comment
+		return new SentimentAnalysis(answer).getTone();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public String getIs_active() {
+		return is_active;
+	}
+
+	public String getCreated_on() {
+		return created_on;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public String getUser_email() {
+		return user_email;
+	}
+
+	public String getAnswerd_on() {
+		return answerd_on;
+	}
+
+	public String getSAR() {
+		return SAR;
+	}
+	
+	
+
+}
